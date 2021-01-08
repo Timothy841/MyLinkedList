b45fa97069a9736286ea002d470bd03cabf9bac7
Timothy <>
2021-01-07 20:15:44 -0500

    Created find method to find the node with index. Used find method to create add(index,value) method.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,14 +1,16 @@
 public class MyLinkedList{
- private int size;
- private Node start,end;
+  private int size;
+  private Node start,end;
 
- public MyLinkedList(){
+  public MyLinkedList(){
    size = 0;
- }
- public int size(){
+  }
+
+  public int size(){
    return size;
- };
- public boolean add(String value){
+  };
+
+  public boolean add(String value){
    if (size == 0){
      start = new Node(value);
      end = start;
@@ -21,17 +23,51 @@ public class MyLinkedList{
      a = end;
      end = b;
      end.setNext(null);
+     end.setPrev(a);
      a.setNext(end);
    }
    size++;
    return true;
   }
- /*
- public boolean add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- */
- public String toString(){
+
+  public boolean add(int index, String value){
+    if (index<0 || index>size){
+      throw new IndexOutOfBoundsException();
+    }
+    Node a = new Node(value);
+    Node b = find(index);
+    if (size == 0){
+      start = new Node(value);
+      end = start;
+      start.setNext(null);
+      start.setPrev(null);
+    }
+    if (index==0){
+      Node c = a;
+      a = start;
+      start = c;
+      start.setPrev(null);
+      start.setNext(a);
+      a.setPrev(start);
+    }
+    else if (index==size){
+      add(value);
+      size--;
+    }
+    else {
+      a.setNext(b);
+      a.setPrev(b.getPrev());
+      b.getPrev().setNext(a);
+      b.setPrev(a);
+    }
+    size++;
+    return true;
+  }
+  /*
+  public String get(int index);
+  public String set(int index, String value);
+  */
+  public String toString(){
    String a = "";
    Node curr = start;
    for (int i = 0;i<size;i++){
@@ -44,6 +80,14 @@ public class MyLinkedList{
      curr = curr.getNext();
    }
    return a;
- }
+  }
+
+  public Node find(int a){
+    Node b = start;
+    for (int i = 0; i<a; i++){
+      b = b.getNext();
+    }
+    return b;
+  }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
